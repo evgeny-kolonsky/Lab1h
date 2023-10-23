b85a4a642beae3e49a7a30a1c980248a171a0ba7
evgeny-kolonsky <126588934+evgeny-kolonsky@users.noreply.github.com>
2023-10-23 17:20:48 +0300

    Fit and compare up-down accelerations
    Accelerations up and down appeared to be not the same with 67% CI.
    This could be explained by small by stat significant value of friction.

--- rinv.m
+++ rinv.m
@@ -1,14 +1,31 @@
 clc
 close all
 
+%  In experimental science, the expression ‘artifact’ is sometimes used to refer 
+% to experimental results which are not manifestations of the natural 
+% phenomena under investigation, but are due to the particular experimental arrangement.
 
+1. Artifacts, Works, and Authors
 
 N = 240
 l = 127 *1e-3 % mm
 L = 127 *1e-2 % cm
 
 x = countB / N * l;
-t = times1;
+t = times1 - times1(1);
+
+fit2 = fit(t, x, 'poly2')
+acc2 = fit2.p1 * 2
+ci = confint(fit2) % confidence intervals
+delta_p1 = (ci(2,1) - ci(1,1))/2
+delta_acc2 = delta_p1 *2
+
+
+
+% plot with uncertainties
+%plot(fit2, 'predobs')
+%hold on 
+%plot(t,x, '.')
 
 %plot(t, x, '.')
 
@@ -18,9 +35,31 @@ dt = t(2*n+1:end) - t(1:end-2*n);
 v = dx./ dt;
 t1 = t(n+1:end-n);
 
-plot(t1,  v, 'r.');
+%plot(t1,  v, 'r.');
 
 
 a = g * h / L
+
+
+%
+xl = x(1:249); tl = t(1:249); xr = x(249:end); tr = t(249:end);
+fit2l = fit(tl, xl, 'poly2')
+fit2r = fit(tr, xr, 'poly2')
+
+plot(t,x, 'b.')
+hold on
+%plot(fit2l, 'predobs')
+plot(fit2r, 'predobs', 0.67)
+plot(fit2l, 'predobs', 0.67)
+hold off
+
+
+acc2l = fit2l.p1 * 2
+acc2r = fit2r.p1 * 2
+
+%friction
+mu = abs(acc2l - acc2r)/2/g
+
+
 
 
